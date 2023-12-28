e74b9bdc3a6bdc66044782f3b2866fdbede9cce6
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 16:12:58 -0500

    Operaciones Basicas

--- Ejercicio_02/src/Main.java
+++ Ejercicio_02/src/Main.java
@@ -1,6 +1,20 @@
 //Realiza operaciones básicas como suma, resta, multiplicación y división.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 22;
+        int num2 = 19;
+
+        double suma, resta, multi;
+        double divi;
+
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multi = num1 * num2;
+        divi = num1 / num2;
+
+        System.out.println("La suma es: " + suma);
+        System.out.println("la resta: " + resta);
+        System.out.println("La multiplicatión es: " + multi);
+        System.out.println("La división es: "+divi);
     }
 }

1fd52ad94b1e1315f1a2578ec49303f17a9add48
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 21:47:48 -0500

    Concatena dos cadenas de texto

---
+++ Ejercicio_09/src/Main.java
@@ -0,0 +1,11 @@
+/*
+    Concatena dos cadenas de texto.
+*/
+public class Main {
+    public static void main(String[] args) {
+        String cadena1 = "Hola mi nombre es: ";
+        String cadena2 = "kelmer";
+
+        System.out.println(cadena1 + cadena2);
+    }
+}

a010a367377ae0a62401557b6d92485d4a7dd97e
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 21:55:16 -0500

    longitud de una cadena de texto

---
+++ Ejercicio_10/src/Main.java
@@ -0,0 +1,9 @@
+/*
+    Encuentra la longitud de una cadena de texto.
+*/
+public class Main {
+    public static void main(String[] args) {
+        String cadena1 = "Hola mundo estoy aprendiendo a programar en Java";
+        System.out.println("Longitud de cadena hay: " +cadena1.length());
+    }
+}

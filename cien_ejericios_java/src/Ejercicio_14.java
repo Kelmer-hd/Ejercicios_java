5542d04d6d78e7cf6627be0e20047a44c2d8db07
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-01 21:30:25 -0500

    imprimir los numeros del 1 al 10

---
+++ cien_ejericios_java/src/Ejercicio_14.java
@@ -0,0 +1,6 @@
+// Realizar un programa que imprima los primeros N números primos.
+public class Ejercicio_14 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ cien_ejericios_java/src/Ejercicio_15.java
@@ -0,0 +1,11 @@
+// Crear un bucle que imprima los números del 1 al 10.
+public class Ejercicio_15 {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 10){
+            System.out.println("Número: " +contador);
+            contador++;
+        }
+    }
+}

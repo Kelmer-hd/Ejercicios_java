d22cf106b528a7a344d319397565b35f0c348135
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-01 20:28:31 -0500

    promedio de tres notas

---
+++ cien_ejericios_java/src/Ejercicio_11.java
@@ -0,0 +1,10 @@
+// Calcular el promedio de tres números.
+public class Ejercicio_11 {
+    public static void main(String[] args) {
+        double nota1 = 12;
+        double nota2 = 20;
+        double nota3 = 15.5;
+        double promedio = Math.round(nota1 + nota2 + nota3) / 3;
+        System.out.println("El promedio de las tres notas es: " + promedio);
+    }
+}

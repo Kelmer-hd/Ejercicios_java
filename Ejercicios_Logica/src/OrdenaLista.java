59e7f30a5d417e17480f654036b56e6e7105e3a9
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-09 20:01:38 -0500

    ordenamiento burbuja

--- Ejercicios_Logica/src/OrdenaLista.java
+++ Ejercicios_Logica/src/OrdenaLista.java
@@ -1,6 +1,24 @@
 // Implementa un algoritmo de ordenamiento (como el de burbuja o selección) para ordenar una lista de números.
+
 public class OrdenaLista {
     public static void main(String[] args) {
-        
+
+        int arregloNumeros [] = {12,20,2,4,17,5,1};
+        int  aux;
+        for ( int i = 0; i < arregloNumeros.length - 1; i++) {
+            for (int j = 0; j < arregloNumeros.length - i - 1; j++) {
+                if (arregloNumeros[j + 1] < arregloNumeros[j]) {
+                    aux = arregloNumeros[j + 1];
+                    arregloNumeros[j + 1] = arregloNumeros[j];
+                    arregloNumeros[j] = aux;
+                }
+            }
+        }
+
+        System.out.print("\nVector ordenado: ");
+        for(int i=0;i<arregloNumeros.length;i++){
+            System.out.print(arregloNumeros[i]+" ");
+        }
+
     }
 }

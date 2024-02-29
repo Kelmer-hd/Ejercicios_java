3ce297a3ef71dc41e3bdf15e00f05e214a880e45
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-02-28 19:53:57 -0500

    asingnacion de variables en java

---
+++ Ejercicio_1/src/Main.java
@@ -0,0 +1,15 @@
+/*
+    Ejercicio 1
+    Crear dos variables de cualquiera de los tipos básicos, dar un valor a la primera y a continuación asigne
+    la segunda a la primera. Imprimir por pantalla las dos variables. Cambiar el valor de la segunda variable
+    y volver a imprimir las variables por pantalla. ¿Qué es lo que ocurre?
+*/
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 10 ;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
